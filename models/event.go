825865285b2e4aadcb859d046695c4cdeec89ddf
RoufDs <four.3687zero@gmail.com>
2025-03-01 09:17:39 +0700

    first commit

---
+++ models/event.go
@@ -0,0 +1,73 @@
+package models
+
+import (
+	"time"
+
+	"www.example.com/booking/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"dateTime" binding:"required"`
+	UserId      int
+}
+
+func (e Event) Save() error {
+	query := `
+	INSERT INTO event (name, description, location, dateTime, userId)
+	VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.LastInsertId()
+	// e.ID = id
+
+	return err
+}
+
+func GetAllEvent() ([]Event, error) {
+	query := "SELECT * FROM event"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEvent(id int64) (*Event, error) {
+	query := "SELECT * FROM event WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
