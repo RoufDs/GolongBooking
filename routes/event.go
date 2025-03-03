22bf4bec811723fe8437efbf5fe95db2a3fdff4f
RoufDs <four.3687zero@gmail.com>
2025-03-03 08:58:46 +0700

    authorization

--- routes/event.go
+++ routes/event.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"www.example.com/booking/models"
+	"www.example.com/booking/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -34,15 +35,26 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event"})
 		return
 	}
 
-	event.ID = 1
 	event.UserId = 1
 
 	err = event.Save()
