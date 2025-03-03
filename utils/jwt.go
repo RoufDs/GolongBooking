4ef10a7bdb1609f1add27ad8a1c909f26c03e6cb
RoufDs <four.3687zero@gmail.com>
2025-03-03 08:58:01 +0700

    verify token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"time"
 
@@ -15,4 +16,33 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Unsupported signing method")
+		}
+
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+	if !tokenIsValid {
+		return errors.New("Token is not valid")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	// if !ok {
+	// 	return errors.New("Invalid token claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
